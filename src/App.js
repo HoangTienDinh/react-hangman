6f5350e9cfb255133f8f7da356874fddead5450b
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-09 22:23:24 -0700

    update css and import FIgure comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Figure from "./components/Figure";
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </div>
   );
 }
