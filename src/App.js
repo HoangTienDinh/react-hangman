1cdcf036d6bbae77bf5366c421f607f9e18b9dd5
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-10 20:13:54 -0700

    fix import for Notification

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import Popup from "./components/Popup";
+import Notification from "./components/Notification";
 
 
 function App() {
