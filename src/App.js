2b450fdfd8641e964061036d75baa1e5ef6d6bc8
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-10 20:38:18 -0700

    pass down props to WrongLetters.js and write functionality for when a wrongletter is typed

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters />
+        <WrongLetters wrongLetters={wrongLetters}/>
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
         <Popup />
         <Notification />
