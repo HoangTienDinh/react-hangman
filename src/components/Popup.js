d99b87c6b66c8a2f5805c610788ff968c6bd2994
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-11 10:49:58 -0700

    finish popup message

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {checkWin} from '../helpers/helpers';
+
 
 const Popup = ({ correctLetters, wrongLetters, setPlayable, selectedWord }) => {
   let finalMessage = "";
   let finalMessageRevealWord = "";
   let playable = true;
-  
+
+  if (checkWin(correctLetters, wrongLetters, selectedWord) === "win") {
+    finalMessage = "Congratulations! You won!";
+    playable = false;
+  } else if (checkWin(correctLetters, wrongLetters, selectedWord) === "lose") {
+    finalMessage = "You lost =(";
+    finalMessageRevealWord = `the word was... ${selectedWord}`;
+    playable = false;
+  }
+
+  useEffect(() => {
+    setPlayable(playable);
+  });
 
   return (
-    <div className="popup-container">
+    <div
+      className="popup-container"
+      style={finalMessage !== "" ? { display: "flex" } : {}}
+    >
       <div className="popup">
         <h2>{finalMessage}</h2>
         <h3>{finalMessageRevealWord}</h3>
