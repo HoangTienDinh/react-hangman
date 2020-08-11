f200a5e6adeff082dd059a77e37c55fcbbb402c0
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-10 20:50:01 -0700

    add handleKeydown functionality for keydown events, and fix bug for reduce function in WrongLetter.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Figure from "./components/Figure";
@@ -7,22 +7,48 @@ import Word from "./components/Word";
 import Popup from "./components/Popup";
 import Notification from "./components/Notification";
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-
 
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
 
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
   return (
     <div className="App">
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters wrongLetters={wrongLetters}/>
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
         <Popup />
         <Notification />
--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -7,7 +7,7 @@ const WrongLetters = ({ wrongLetters }) => {
         {wrongLetters.length > 0 && <p>Wrong</p>}
         {wrongLetters.map((letter, i) => (
           <span key={i}>{letter}</span>
-        )).reduce((prev, curr) => prev === null ? [curr] : [prev, ',', [curr], null])
+        )).reduce((prev, curr) => prev === null ? [curr] : [prev, ',', curr], null)
         }
       </div>
     </div>
