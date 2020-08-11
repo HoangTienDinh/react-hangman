e3fb33f90a59806fcc8e216420b2fa872d7175ae
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-10 20:10:37 -0700

    create WrongLetters component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './App.css';
 import Header from "./components/Header";
 import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
 
-import './App.css';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="game-container">
         <Figure />
+        <WrongLetters />
       </div>
     </div>
   );
